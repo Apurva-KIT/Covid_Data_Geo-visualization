eaf4ef304942f25e5a2d4622009645abc03c7ae9
Apurva Yadav <Apurva-KIT@users.noreply.github.com>
2020-09-17 11:26:57 +0530

    Final_1 Changes

--- world_Covid_DeathCases_HeatMap.py
+++ world_Covid_DeathCases_HeatMap.py
@@ -20,49 +20,29 @@ covid1 = pd.json_normalize(covid1['Countries'],sep = ",")
 df = pd.DataFrame(covid1) #to be able to manipulate the data
 covid2 = df.drop(columns=['CountryCode','Slug','Date'], axis=1)
 
-
 #code for map
-m = folium.Map(tiles="Stamen Terrain", min_zoom = 1.5)
-
-# GeoJson file
-url = 'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data'
-country_shapes = f'{url}/world-countries.json'
-
-
-folium.Choropleth(
-    geo_data = country_shapes,
-    min_zoom = 3,
-    name = 'Covid-19',
-    data = covid2,
-    columns = ['Country','TotalConfirmed'],
-    key_on = 'feature.properties.name',
-    fill_color = 'OrRd',
-    nan_fill_color = 'black',
-    legend_name = 'Total Confirmed Covid Cases',
-).add_to(m)
-
-
-
-#Generate Circular Markers
-covid2.update(covid2['TotalConfirmed'].map('Total Confirmed:{}'.format))
-covid2.update(covid2['TotalRecovered'].map('Total Recovered:{}'.format))
+m1 = folium.Map(tiles="StamenToner", min_zoom = 2)
 
 # coordinates = pd.read_csv('https://raw.githubusercontent.com/Apurva-KIT/covid-map/master/country-coordinates-world.csv')
 coordinates = pd.read_csv('https://raw.githubusercontent.com/VinitaSilaparasetty/covid-map/master/country-coordinates-world.csv')
 
-print(coordinates)
-
 covid_final = pd.merge(covid2,coordinates, on = 'Country')
+
+#Heat Map
+deaths = covid_final['TotalDeaths'].astype(float)
+lat=covid_final['latitude'].astype(float)
+lon=covid_final['longitude'].astype(float)
+m1.add_child(HeatMap(zip(lat,lon,deaths),radius=0))
+
+#Generate Circular Markers
+covid_final.update(covid_final['TotalDeaths'].map('Total Deaths:{}'.format))
 
 def plotDot(point):
     folium.CircleMarker(location=[point.latitude,point.longitude],
-                        radius=5,
-                        weight=2,
-                        popup=[point.Country, point.TotalConfirmed, point.TotalRecovered],
-                        fill_color='#000000').add_to(m)
+                        radius = 5,
+                        weight = 2,
+                        popup = [point.Country, point.TotalDeaths],
+                        fill_color='#000000').add_to(m1)
 
 covid_final.apply(plotDot, axis=1)
-
-m.save(outfile='map.html')
-
-print(covid1)
+m1.save(outfile='HeatMap.html')
